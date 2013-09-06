a79b3e7b446b92637d4dddce9de89b157551e793
Bjarke Walling <bwp@bwp.dk>
2013-09-06 15:10:33 +0200

    Attaching helpers in singleton module.

--- AssetStream.js
+++ AssetStream.js
@@ -5,5 +5,6 @@ AssetStream.Source = require('./Source');
 AssetStream.Transform = require('./Transform');
 AssetStream.Destination = require('./Destination');
 
+AssetStream.helper = require('./helper/index');
 AssetStream.source = require('./source/index');
 AssetStream.transform = require('./transform/index');
--- helper/index.js
+++ helper/index.js
@@ -1,2 +1,9 @@
 
+exports.assetHash = require('./assetHash');
+exports.contentSize = require('./contentSize');
+exports.deserializeAsset = require('./deserializeAsset');
+exports.isTextAsset = require('./isTextAsset');
+exports.isType = require('./isType');
+exports.serializeAsset = require('./serializeAsset');
 exports.streamToString = require('./streamToString');
+exports.transformCache = require('./transformCache');
