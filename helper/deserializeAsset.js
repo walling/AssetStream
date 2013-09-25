5605061f350bf34657aa8b3de8f006a07abcded6
Bjarke Walling <bwp@bwp.dk>
2013-09-25 14:48:29 +0200

    Bugfix in deserializer.

--- helper/deserializeAsset.js
+++ helper/deserializeAsset.js
@@ -6,7 +6,7 @@ function deserializeAsset(asset) {
 
 	for (var key in asset.content) {
 		if (asset.content[key] && asset.content[key].$buffer) {
-			asset.content[key] = new Buffer(asset.content[key].$buffer);
+			asset.content[key] = new Buffer(asset.content[key].$buffer, 'base64');
 		}
 	}
 
